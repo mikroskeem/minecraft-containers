0f7ce0fdf7ae30739e6d32233a702bdff81493b4
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-05-11 02:32:28 +0300

    Debug shell support

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,12 @@ java -version
 INTERNAL_IP="$(ip route get 1 | awk '{print $NF;exit}')"
 export INTERNAL_IP
 
+# Debug shell
+if [ -f .debugshell ]; then
+    echo ">>> Do not forget to delete /home/container/.debugshell to launch your server normally."
+    exec /bin/sh -i
+fi
+
 # Replace Startup Variables
 MODIFIED_STARTUP="$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")"
 echo ":/home/container$ ${MODIFIED_STARTUP}"
